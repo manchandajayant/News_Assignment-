e0e6b4c5f4f9f79dab9de42f21c80f3d75f67aba
manchandajayant <maanjayant@gmail.com>
2020-07-10 17:23:54 +0200

    api key hidden, moore styling added

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Row, Col } from "react-bootstrap";
+
 import Layout from "./Layout";
+const API_KEY = `${process.env.REACT_APP_NEWS_API_KEY}`;
+console.log(API_KEY);
 const NewsFetched = () => {
   const [data, setdata] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
-        "http://newsapi.org/v2/everything?q=football&from=2020-07-09&sortBy=publishedAt&apiKey=96433d611b85480f98304d80e9f10078"
+        `http://newsapi.org/v2/everything?q=football&from=2020-07-09&sortBy=publishedAt&apiKey=${API_KEY}`
       );
 
       //console.log("res", result);
@@ -17,10 +19,10 @@ const NewsFetched = () => {
     fetchData();
   }, []);
   //console.log(data);
-  let articleCard = data.map((articleObject) => (
-    <Col sm={4}>
+  let articleCard = data.map((articleObject, index) => (
+    <div className="col-md-4" key={index}>
       <Layout {...articleObject} />
-    </Col>
+    </div>
   ));
 
   if (!data) {
@@ -28,7 +30,10 @@ const NewsFetched = () => {
   } else {
     return (
       <div>
-        <Row>{articleCard}</Row>
+        <h5>Your Daily Football News Updates</h5>
+        <div className="container-fluid d-flex justify-content-center">
+          <div className="row">{articleCard}</div>
+        </div>
       </div>
     );
   }
--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,19 +1,29 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
-
+// import { Card, Button, Container, CardDeck, Image } from "react-bootstrap";
+import "../card-style.css";
 const Layout = (props) => {
   const { title, description, urlToImage, url } = props;
   //console.log(props);
   return (
-    <div>
-      <Card style={{ width: "18rem" }}>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{description}</Card.Text>
-        <Card.Img variant="top" src={urlToImage} />
-        <Card.Body>
-          <Button variant="primary">Go to the Article</Button>
-        </Card.Body>
-      </Card>
+    <div className="card-group">
+      <div className="card">
+        <div className="overflow">
+          <a href={url} target="_empty">
+            <img
+              src={urlToImage}
+              className="card-img-top"
+              alt="nothing to show"
+            />
+          </a>
+        </div>
+        <div className="card-body text dark">
+          <h4 className="card-title">{title}</h4>
+        </div>
+        <p className="card-text text-secondary">{description}</p>
+        <a href={url} className="btn btn-outline" target="_empty">
+          Go to the Article
+        </a>
+      </div>
     </div>
   );
 };
